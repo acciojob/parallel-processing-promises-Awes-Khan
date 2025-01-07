7b521063a6b1f692d548e46f8323397e3e48615f
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2025-01-07 19:03:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,42 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+let promises =[];
+for(i=0;i<images.length;i++){
+	
+	promises.push(
+	  new Promise((resolve, reject)=>{
+  		fetch(images[i].url)
+  			.then( (response)=>{
+  				if(!response.ok){
+  					throw new Error(`Failed to load image's URL: ${images[i].url}`);
+  				} else {
+  					return response.blob()
+  				}
+  			}).then((blob)=>{
+  				const imageUrl = URL.createObjectURL(blob);
+  				resolve(imageUrl);
+  			}).catch((error)=>{
+  				reject(error);
+  			});
+  	})
+
+		// downloadImage(images[i].url)
+	);
+}
+
+
+btn.addEventListener('click', () => {
+Promise.all(promises)
+	.then((imageUrls)=>{
+		imageUrls.forEach(url => {
+      const img = document.createElement('img');
+      img.src = url;
+      output.appendChild(img);
+    });
+	
+  })
+  .catch((error)=>{
+    alert(error);
+  });
+});
